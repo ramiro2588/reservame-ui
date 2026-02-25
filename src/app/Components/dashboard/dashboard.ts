648b94b31a49141c95875a7275142dca977a45d5
Ramiro Gonzalez <76759631+ramiro2588@users.noreply.github.com>
2026-02-25 17:44:28 -0300

    dashboard reset and search section created

--- src/app/Components/dashboard/dashboard.ts
+++ src/app/Components/dashboard/dashboard.ts
@@ -2,17 +2,14 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { Search } from './search/search';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatInputModule, MatButtonModule, MatIconModule, Search],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
-  companys = [
-    { name: 'Barberia El Estilo', location: 'Montevideo', description: 'Barberia de calidad con servicios premium.', rating: 4.5, img: 'https://tse2.mm.bing.net/th/id/OIP.pRAkPVoy6UAse_44qlc70AHaE8?cb=defcache2&defcache=1&rs=1&pid=ImgDetMain&o=7&rm=3' },
-    { name: 'Dentista Dr. García', location: 'Maldonado', description: 'Clínica dental con atención personalizada.', rating: 4.8, img: '' },
-    { name: 'Gimnasio Fitness', location: 'Lavalleja', description: 'Gimnasio con equipos modernos y entrenadores certificados.', rating: 4.2, img: 'https://tse2.mm.bing.net/th/id/OIP.pRAkPVoy6UAse_44qlc70AHaE8?cb=defcache2&defcache=1&rs=1&pid=ImgDetMain&o=7&rm=3' },
-  ]
+
 }
